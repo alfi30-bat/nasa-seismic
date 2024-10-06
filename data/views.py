787eef9acd65d671a32b489e1adec372d958f22f
alfi30-bat <78789080+alfi30-bat@users.noreply.github.com>
2024-10-06 23:10:31 +0530

    Update views.py

--- data/views.py
+++ data/views.py
@@ -14,40 +14,7 @@ cat_directory = '../data/lunar/training/catalogs/'
 cat_file = cat_directory + 'apollo12_catalog_GradeA_final.csv'
 cat = pd.read_csv(cat_file)
 data = []
-def data_exe(request):
-    i = 0 
-    k = 0
-    while i < 1:
-        #row = cat.iloc[7]
-        row = cat.iloc[i]
-        arrival_time = datetime.strptime(row['time_abs(%Y-%m-%dT%H:%M:%S.%f)'],'%Y-%m-%dT%H:%M:%S.%f')
-        test_filename = row.filename
 
-        data_directory = '../data/lunar/training/data/S12_GradeA/'
-        mseed_file = f'{data_directory}{test_filename}.mseed'
-        st = read(mseed_file)
-
-        # Set the minimum frequency
-        minfreq = 0.5
-        maxfreq = 1.0
-
-        # Going to create a separate trace for the filter data
-        st_filt = st.copy()
-        st_filt.filter('bandpass',freqmin=minfreq,freqmax=maxfreq)
-        tr_filt = st_filt.traces[0].copy()
-        tr_times_filt = tr_filt.times()
-        tr_data_filt = tr_filt.data
-        le = len(tr_data_filt)
-        while k < le:
-            data.append(tr_data_filt[k])
-            #dataa = [round(abs(num), 2) for num in data]
-            k = k+1 
-            #print(data)
-        i=i+1
-        
-
-    #print(data)
-    return render(request, "data/index.html", {'dataa':json.dumps(data), "length":le})
 
 def moon_data(request):
     # Initialize an empty dictionary, list, and row counter
